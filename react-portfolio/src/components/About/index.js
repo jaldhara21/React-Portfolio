65d543ad752337fac0600d78225408befff4080e
jaldhara.patel <jaldharapatel91@gmail.com>
2023-09-05 22:43:43 -0400

    Aded profilr pic and about

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profileImage from "";
+import profileImage from "../../assets/Profile/profile_pic.jpg";
 function About() {
   return (
     <section id="about-me" className="my-5 intro">
@@ -13,7 +13,16 @@ function About() {
           <div class="intro-img">
             <img src={profileImage} style={{ width: "100%" }} alt="profile" />
           </div>
-          
+          <p>
+  Hello, I'm Jaldhara Patel, a dedicated Full Stack Developer based in North Carolina. My journey began with a Bachelor's degree in Nursing, which I completed in India back in 2013. I initially pursued a career in healthcare, working at a nursing home, but the COVID-19 pandemic prompted a significant change in my career path.
+</p>
+<p>
+  During these challenging times, I made a life-altering decision to pivot into the tech world. I embarked on a web development bootcamp to acquire the skills needed to thrive in the ever-evolving digital landscape. This transition has opened up exciting opportunities for me to work remotely and contribute to the world of technology.
+</p>
+<p>
+  Beyond coding, I have a passion for cooking and savoring quality time with my family. Exploring diverse cuisines and creating delicious dishes brings me immense joy. My family and I cherish these moments together, which have become even more meaningful in recent times.
+</p>
+
         </div>
       </div>
     </section>
